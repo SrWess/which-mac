cc064d05e46518fdee1c09655d802fdc85ed33bd
Wesley Anjos <wanjos.andrade@gmail.com>
2020-11-24 01:17:50 -0300

    Implementação da validação de caracteres

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const macCompany = document.querySelector('.company-result')
 const macCountry = document.querySelector('.country-result')
 const macValidade = document.querySelector('.valid-result')
 
-async function handleAddresMac(event) {
-  const macValue = event.target.value
+async function handleAddresMac(e) {
+  const regexp = /([a-f0-9]{2})([a-f0-9]{2})/i
+  let macFormat = e.target.value.replace(/[^a-f0-9]/ig, "");
+
+  while (regexp.test(macFormat)) {
+    macFormat = macFormat.replace(regexp, '$1' + ':' + '$2');
+  }
+  e.target.value = macFormat.slice(0, 17);
 
   try {
-    const macResponse = await fetch(`https://api.macaddress.io/v1?apiKey=at_snQfQFugx70p1uAqMPp4AuJVnsK2k&output=json&search=${macValue}`)
+    const macResponse = await fetch(`https://api.macaddress.io/v1?apiKey=at_snQfQFugx70p1uAqMPp4AuJVnsK2k&output=json&search=${macFormat}`)
     const macJson = await macResponse.json()
 
     macResult.innerText = `${(macJson.macAddressDetails.searchTerm)}`
@@ -16,11 +22,11 @@ async function handleAddresMac(event) {
     macCountry.innerText = `${(macJson.vendorDetails.countryCode)}`
     macValidade.innerText = `${(macJson.macAddressDetails.isValid)}`
 
-    console.log(macJson.macAddressDetails.isValid);
   } catch(err) {
-    console.log(err);
+    console.log('Mac inválido');
   }
 }
+
 
 inputMac.addEventListener('input', handleAddresMac)
 
