d97a697bac5436eb932796e7797fa4c45928fd5f
Wesley Anjos <wanjos.andrade@gmail.com>
2020-11-24 12:53:44 -0300

    Validação do MAC e Implementação msg de erro

--- script.js
+++ script.js
@@ -5,25 +5,28 @@ const macCountry = document.querySelector('.country-result')
 const macValidade = document.querySelector('.valid-result')
 
 async function handleAddresMac(e) {
-  const regexp = /([a-f0-9]{2})([a-f0-9]{2})/i
-  let macFormat = e.target.value.replace(/[^a-f0-9]/ig, "");
-
-  while (regexp.test(macFormat)) {
-    macFormat = macFormat.replace(regexp, '$1' + ':' + '$2');
-  }
-  e.target.value = macFormat.slice(0, 17);
-
   try {
-    const macResponse = await fetch(`https://api.macaddress.io/v1?apiKey=at_snQfQFugx70p1uAqMPp4AuJVnsK2k&output=json&search=${macFormat}`)
-    const macJson = await macResponse.json()
+    const regexp = /([a-f0-9]{2})([a-f0-9]{2})/i
+    let macFormat = e.target.value.replace(/[^a-f0-9]/ig, "");
+  
+    while (regexp.test(macFormat)) {
+      macFormat = macFormat.replace(regexp, '$1' + ':' + '$2');
+    }
+    e.target.value = macFormat.slice(0, 17);
+  
+    if (macFormat.length >= 7) {
+      const macResponse = await fetch(`https://api.macaddress.io/v1?apiKey=at_snQfQFugx70p1uAqMPp4AuJVnsK2k&output=json&search=${macFormat}`)
+      const macJson = await macResponse.json()
+    
+      macResult.innerText = `${(macJson.macAddressDetails.searchTerm)}`
+      macCompany.innerText = `${(macJson.vendorDetails.companyName)}`
+      macCountry.innerText = `${(macJson.vendorDetails.countryCode)}`
+      macValidade.innerText = `${(macJson.macAddressDetails.isValid)}`
+    }
 
-    macResult.innerText = `${(macJson.macAddressDetails.searchTerm)}`
-    macCompany.innerText = `${(macJson.vendorDetails.companyName)}`
-    macCountry.innerText = `${(macJson.vendorDetails.countryCode)}`
-    macValidade.innerText = `${(macJson.macAddressDetails.isValid)}`
+  } catch (err) {
+    macResult.innerHTML = `<span class="mac-err">Não conseguimos carregar os dados.</span>`;
 
-  } catch(err) {
-    console.log('Mac inválido');
   }
 }
 
